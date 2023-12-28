eab2d6bc5ba2b9d172b0dc6da7bf74a8f05d00b2
Grigorii-Loskutov <greg3mbpp@yandex.ru>
2023-12-28 13:12:06 +0300

    Update indexator.cpp
    Оптимизирована структура try - catch

--- Spider/indexator.cpp
+++ Spider/indexator.cpp
@@ -21,43 +21,76 @@ std::set<std::string> indexator(database& DB, std::string inLink) {
 		if (client.performGetRequest(inLink, "80", "/", 5) == 0)
 		{
 			response = client.getData();
-		}
-		ParcerHTML parcerHTML(response, inLink);
-		Links = parcerHTML.getLinks();
-		Frequencies = parcerHTML.getFrequencies();
+			ParcerHTML parcerHTML(response, inLink);
+			Links = parcerHTML.getLinks();
+			Frequencies = parcerHTML.getFrequencies();
+			// Выведем найденные ссылки
+			std::cout << "На странице: " << inLink << " найдены ссылки:" << std::endl;
+			for (const auto& line : Links) {
+				std::cout << line << std::endl;
+			}
 
-		// Выведем найденные ссылки
-		std::cout << "На странице: " << inLink << " найдены ссылки:" << std::endl;
-		for (const auto& line : Links) {
-
-			std::cout << line << std::endl;
-		}
-
-
-		// Запишем адресс страницы, которую проиндексировали
-		try
-		{
-			DB.link_add(inLink);
-		}
-		catch (const std::exception& ex) {
-			std::cout << "\n Try to add new URL in database " + inLink << ": ";
-			std::string except = ex.what();
-			std::cout << "\n" << except;
-		}
-
-		unsigned int counter = 0;
-		for (const auto& pair : Frequencies) {
-			//std::cout << counter << ". " << pair.first << ": " << pair.second << std::endl;
-			//++counter;
-			// Запись в database под отдельный try
-			try {
-				DB.word_add(pair.first);
+			// Добавим в базу адресс страницы, которую проиндексировали (если такого адреса там ещё нет)
+			try
+			{
+				DB.link_add(inLink);
 			}
 			catch (const std::exception& ex) {
-				std::cout << "\n Try to add new word in database: ";
+				std::cout << "\n Try to add new URL in database " + inLink << ": ";
 				std::string except = ex.what();
 				std::cout << "\n" << except;
 			}
+
+			// Добавим в базу слова, найденные на странице (если найденные слова уникальные)
+			for (const auto& pair : Frequencies) {
+
+				// Запись в database под отдельный try
+				try {
+					DB.word_add(pair.first);
+				}
+				catch (const std::exception& ex) {
+					std::cout << "\n Try to add new word in database: ";
+					std::string except = ex.what();
+					std::cout << "\n" << except;
+				}
+			}
+
+			// Вносим в базу данных частоты слов для определенной страницы
+				// Получим id страницы, которую индексируем
+			try {
+				link_id = DB.getLinkId(inLink);
+
+				// Получим таблицу слов с id целиком
+				try {
+					WordIdPair = DB.getWordId();
+				}
+				catch (const std::exception& ex) {
+					std::cout << "\n Try to get words id: ";
+					std::string except = ex.what();
+					std::cout << "\n" << except;
+				}
+				//Заполним таблицу частот, если успешно получили ID страницы
+				for (const auto& pair : Frequencies) {
+					//std::cout << counter << ". " << pair.first << ": " << pair.second << std::endl;
+					//++counter;
+					int wordFrequency = pair.second;
+					int wordId = WordIdPair[pair.first];
+					try {
+						DB.frequency_add(link_id, wordId, wordFrequency);
+					}
+					catch (const std::exception& ex) {
+						std::cout << "\nTry to add frequency for word " << pair.first << ": ";
+						std::string except = ex.what();
+						std::cout << "\n" << except;
+					}
+				}
+			}
+			catch (const std::exception& ex) {
+				std::cout << "\nTry to get link ID for " << inLink << ": ";
+				std::string except = ex.what();
+				std::cout << "\n" << except;
+			}
+			
 		}
 	}
 	catch (const std::exception& ex) {
@@ -65,51 +98,6 @@ std::set<std::string> indexator(database& DB, std::string inLink) {
 		std::string except = ex.what();
 		std::cout << "\n" << except;
 
-	}
-
-	// Вносим в базу данных частоты слов для определенной страницы
-
-	// Получим таблицу слов с id целиком
-	try {
-		WordIdPair = DB.getWordId();
-	}
-	catch (const std::exception& ex) {
-		std::cout << "\n Try to get words id: ";
-		std::string except = ex.what();
-		std::cout << "\n" << except;
-	}
-
-	// Выведем таблицу
-	//unsigned int counter = 0;
-	//for (const auto& pair : WordIdPair) {
-	//	std::cout << counter << ". " << pair.first << ": " << pair.second << std::endl;
-	//	++counter;
-	//}
-
-	// Получим id страницы, которую индексируем
-	try {
-		link_id = DB.getLinkId(inLink);
-		//std::cout << inLink << " id = " << link_id << std::endl;
-	}
-	catch (const std::exception& ex) {
-		std::cout << "\nTry to get link id for " << inLink << ": ";
-		std::string except = ex.what();
-		std::cout << "\n" << except;
-	}
-	//Заполним таблицу частот
-	for (const auto& pair : Frequencies) {
-		//std::cout << counter << ". " << pair.first << ": " << pair.second << std::endl;
-		//++counter;
-		int wordFrequency = pair.second;
-		int wordId = WordIdPair[pair.first];
-		try {
-			DB.frequency_add(link_id, wordId, wordFrequency);
-		}
-		catch (const std::exception& ex) {
-			std::cout << "\nTry to add frequency for word " << pair.first << ": ";
-			std::string except = ex.what();
-			std::cout << "\n" << except;
-		}
 	}
 	std::cout << "\nИндексатор отработал и не упал" << std::endl;
 	return Links;
