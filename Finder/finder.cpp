0e29b9500de75bc0bee5c667680122062bc4db0f
Grigorii-Loskutov <greg3mbpp@yandex.ru>
2024-01-02 00:10:02 +0300

    Update finder.cpp

--- Finder/finder.cpp
+++ Finder/finder.cpp
@@ -1,7 +1,10 @@
-#include "finder.h"
+#include <vector>
+#include <string>
 #include <iostream>
-#include <string.h>
-#include <exception>
+#include <iterator>
+#include <regex>
+#include <boost/locale.hpp>
+#include <boost/locale/conversion.hpp>
 #include "../Spider/database.h"
 
 std::vector<std::string> finder(std::string inSeachString) {
@@ -9,6 +12,47 @@ std::vector<std::string> finder(std::string inSeachString) {
 
 	// Создадим подключение к базе данных
 	database DB;
+
+	// Удаление знаков препинания и скобок
+	std::regex pattern_punctuation(R"([[:punct:]()])");
+	inSeachString = std::regex_replace(inSeachString, pattern_punctuation, " ");
+
+	// Удаления кавычек ("), одинарных (') и дефисов (-)
+	std::regex pattern_remove_quotes_and_dashes(R"([\"'-])");
+	inSeachString = std::regex_replace(inSeachString, pattern_remove_quotes_and_dashes, "");
+
+	// Удаление чисел (всех слов с числами)
+	std::regex pattern_numbers("\\b\\w*\\d+\\w*\\b");
+	inSeachString = std::regex_replace(inSeachString, pattern_numbers, " ");
+
+	// Удаление лишних пробелов
+	std::regex SPACEpattern(R"(\s+)");
+	inSeachString = std::regex_replace(inSeachString, SPACEpattern, "_");
+
+	// Переведем в нижний регистр
+	boost::locale::generator gen;
+	std::locale loc = gen(""); // Используем локаль по умолчанию
+	inSeachString = boost::locale::to_lower(inSeachString, loc);
+
+	// Заполним set для хранения слов для поиска
+	std::set<std::string> setInWords;
+	unsigned int cut_end_pos{ 0 };
+	unsigned int cut_start_pos{ 0 };
+	for (unsigned int iter = 0; iter < inSeachString.length(); ++iter) {
+		if (inSeachString[iter] == '_') {
+			cut_end_pos = iter;
+			std::string word = inSeachString.substr(cut_start_pos, cut_end_pos - cut_start_pos);
+			if (word.length() >= 4) {
+				setInWords.insert(word);
+			}
+			cut_start_pos = iter + 1;
+		}
+	}
+	std::cout << "\nСлова для поиска: \n";
+	for (const auto& word : setInWords) {
+		std::cout << word << std::endl;
+	}
+	
 	try {
 		DB.SetConnection("localhost", "indexator", "postgres", "cfhvf810", 5432);
 		try {
